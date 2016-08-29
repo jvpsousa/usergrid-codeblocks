2f7bee41578496ca260302af55154e9cab6ebed0
João Vitor <jvpsousa@gmail.com>
2016-08-29 03:06:54 -0300

    primeira versão

---
+++ usergrid-codeblocks.js
@@ -0,0 +1,116 @@
+var restify  = require('restify');
+var request  = require('request');
+
+var config = null;
+
+var server = restify.createServer();
+
+server
+	.use(restify.fullResponse())
+	.use(restify.bodyParser())
+	.use(restify.CORS({
+		origins:['*']
+	}));
+
+var routes = {};
+
+
+function _route(route, method, authRequired, requiredRole, callback) {
+
+	var baseRoute = traitUrl(route);
+	routes[baseRoute] = {
+		authRequired: authRequired,
+		requiredRole: requiredRole
+	};
+
+	var routeFilter = function(req, res, next) {
+		
+		var requestRoute = traitUrl(req.url);
+
+		
+
+		if(routes[requestRoute].authRequired) {
+
+			var baseUrl = req.body.usergridConfig.baseUrl+'/'+req.body.usergridConfig.orgId+'/'+req.body.usergridConfig.appId;
+			console.log('baseURL: '+baseUrl);
+
+			request({
+				method: 'GET',
+				uri: baseUrl+'/users/me',
+				headers: {
+					'Authorization': 'Bearer '+ req.body.usergridConfig.token
+				}
+			}, function(error, response, body) {
+
+				body = JSON.parse(body);
+
+				if(error || body.error) {
+				
+					res.contentType = 'json';
+					res.send(403, {error: 'You must be logged in to access this route !'});
+					res.end();
+				
+				} else {
+
+					if(routes[requestRoute].requiredRole != null) {
+						
+						var rolesUrl = baseUrl+body.entities[0].metadata.sets.rolenames;
+
+						request({
+							method: 'GET',
+							uri: rolesUrl,
+							headers: {
+								'Authorization': 'Bearer '+ req.body.usergridConfig.token
+							} 
+						}, function(error, response, body) {
+
+							body = JSON.parse(body);
+							
+							if(error || body.error) {
+								
+								res.contentType = 'json';
+								res.send(500, {error: 'Ops, something wrong !'});
+								res.end();
+
+							} else {
+
+								if(body.entities.length == 1 && body.entities[0].name == routes[requestRoute].requiredRole) {
+									callback(req, res, next);
+								} else {
+
+									res.contentType = 'json';
+									res.send(403, {error: 'You have no permission to access this route !'});
+									res.end();
+								
+								}
+							}
+
+						});
+
+					}
+				}
+
+			});
+			
+		}
+	}
+
+	if(method == 'POST') server.post(route, routeFilter);
+
+}
+
+function _run(port, callback) {
+	server.listen(port, callback);
+}
+
+function traitUrl(url) {
+	var split = url.split('/');
+	return '/'+split[1];
+}
+
+var usergridCodeblocks = {
+	route:  _route,
+	run: _run
+}
+
+module.exports = usergridCodeblocks;
